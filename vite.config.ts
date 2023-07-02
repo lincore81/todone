4f53a0c3b06a0eff0b8ec9644f75cbcbc449f1f6
lincore81 <ghspam.99.lbh81@spamgourmet.com>
2023-07-02 20:06:40 +0200

    Fix production version.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as path from "path";
 
 // https://vitejs.dev/config/
@@ -14,4 +14,4 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
     }
   }
-})
+});
